14593d0998a34cff5c3f92385be999ea72523789
rsparikh157 <rutvsparikh@gmail.com>
2025-08-12 17:34:34 +0530

    Add files via upload

---
+++ assignment 2 Q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+int a,b;
+printf("Enter 2 numbers a and b\n");
+scanf("%d %d", &a, &b);
+if (a>b)
+{
+printf("a is larger than b");
+}
+else
+    {
+printf("a is smaller than b");
+}
+
+
+}
